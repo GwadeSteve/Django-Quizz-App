61725a52b860877b4fc444cc156378ca3ff95704
GSteve12 <gwadesteve12@gmail.com>
2023-10-11 15:06:53 +0100

    Project SetUp

---
+++ Quiz_App/Quiz/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from Quiz import views as quiz_views  # Import your app's views
+
+urlpatterns = [
+    
+]
---
+++ Quiz_App/Users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from Users import views as users_views  # Import your Users app views
+
+app_name = 'Users'
+
+urlpatterns = [
+    
+]
