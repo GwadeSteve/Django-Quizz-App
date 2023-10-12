f1a90a3713cfd4ce0c89697e4d8a2e0a59634202
GSteve12 <gwadesteve12@gmail.com>
2023-10-12 16:42:09 +0100

    project backend is done fully

--- Quiz_App/Quiz/urls.py
+++ Quiz_App/Quiz/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from Quiz import views
 
+app_name = "Quiz"
+
 urlpatterns = [
     path('',views.main,name="home"),
 ]
--- Quiz_App/Quiz/views.py
+++ Quiz_App/Quiz/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render,redirect
 from Quiz.models import Question
-from Users.forms import CustomUserLoginForm
 
 def main(request):
     user = request.user
-    if request.method == 'GET':
-        if user.is_authenticated:
-            questions = Question.objects.all()
-            context = {
-                'questions': questions,
-            }
-            return render(request, 'Quiz/quiz.html',context)
-        else:
-            return redirect('Users:login')  
-    else:
+    if not user.is_authenticated:
+        return redirect('Users:login')
+    if request.method == 'POST':
         Score = 0
-        Question.objects.all()
-        users_answers = request.POST
-        time_taken = int(request.POST.get('time_taken', 0)) 
-        for question_id, user_answer in users_answers.items():
-            try:
-                question = Question.objects.get(id=question_id)
-            except Question.DoesNotExist:
-                continue
+        time_taken = int(request.POST.get('time_taken', 0))
+        users_answers = request.POST.copy()  # Create a copy of the POST data
+        # Remove the CSRF token from the copied data
+        if 'csrfmiddlewaretoken' in users_answers:
+            del users_answers['csrfmiddlewaretoken']
+        for question in Question.objects.all():
+            question_id = str(question.id)
+            user_answer = users_answers.get('question_' + question_id)  # Get the selected answer
+
             if user_answer == question.answer:
                 Score += 10
         user.score = Score
         user.time_taken = time_taken
-        user = user.save()
-        print(f"Score = {Score} and time taken = {time_taken}")
+        user.save()
+        print(f"Score = {Score} and time taken = {time_taken}s")
         context = {
             'score': Score,
             'time_taken': time_taken,
         }
-        return render(request,'Quiz/result.html',context)
+        return render(request, 'Quiz/result.html', context)
+    else:
+        questions = Question.objects.all()
+        context = {
+            'questions': questions,
+        }
+        return render(request, 'Quiz/quiz.html', context)
+
